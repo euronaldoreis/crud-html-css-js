985966fac1906249fd945c1caa99df3ef61fa557
Ronaldo Reis <reeisfst@gmail.com>
2020-05-22 23:29:30 -0400

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+window.addEventListener('load', start);
+
+var globalNames = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco'];
+var inputName = null;
+var currentIndex = null;
+var isEditing = false;
+
+function start() {
+  inputName = document.querySelector('#inputName');
+  preventFormSubmit();
+  activateInput();
+  render();
+}
+
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  var form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput() {
+  function insertName(newName) {
+    globalNames.push(newName);
+  }
+
+  function updateName(newName) {
+    globalNames[currentIndex] = newName;
+  }
+  function handleTyping(event) {
+    if (event.key === 'Enter' && event.target.value.trim() !== '') {
+      if (isEditing) {
+        updateName(event.target.value);
+      } else {
+        insertName(event.target.value);
+      }
+      render();
+      isEditing = false;
+      clearInput();
+    }
+  }
+
+  inputName.focus();
+  inputName.addEventListener('keyup', handleTyping);
+}
+
+function render() {
+  function createDeleteButton(index) {
+    function deleteName() {
+      //Remove elementos do vetor
+      globalNames.splice(index, 1);
+      render();
+    }
+    var button = document.createElement('button');
+    button.classList.add('deleteButton');
+    button.textContent = 'x';
+    button.addEventListener('click', deleteName);
+    return button;
+  }
+
+  function createSpan(name, index) {
+    function editItem() {
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+      currentIndex = index;
+    }
+    var span = document.createElement('span');
+    span.classList.add('clickable');
+    span.textContent = name;
+    span.addEventListener('click', editItem);
+
+    return span;
+  }
+  var divNames = document.querySelector('#names');
+  divNames.innerHTML = '';
+
+  //Criar ul
+  //fazer n li's. conforme tamanho do globalNames
+  var ul = document.createElement('ul');
+
+  for (var i = 0; i < globalNames.length; i++) {
+    var currentName = globalNames[i];
+
+    var li = document.createElement('li');
+    var button = createDeleteButton(i);
+    var span = createSpan(currentName, i);
+
+    li.appendChild(button);
+    li.appendChild(span);
+    ul.appendChild(li);
+  }
+
+  divNames.appendChild(ul);
+  clearInput();
+}
+
+function clearInput() {
+  inputName.value = '';
+  inputName.focus();
+}
